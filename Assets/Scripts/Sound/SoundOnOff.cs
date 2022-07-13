c553faf95e602eae89342435cf245e0b6984a6c4
qkrdngur <pwh060720@gmail.com>
2022-07-13 20:25:25 +0900

    scene
    사운드 추가 및 옵션 추가
    패널 움직임 상호작용

---
+++ Assets/Scripts/Sound/SoundOnOff.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundOnOff : MonoBehaviour
+{
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void ToggleAudioVolume()
+    {
+        AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;   
+        
+        if(AudioListener.volume <= 0)
+        {
+            anim.SetTrigger("Off");
+        }
+        else
+        {
+            anim.SetTrigger("On");
+        }
+    }
+}
